983d559be761d2f30ffba08041c7a320d60a0b28
asstelite <kobukstr@gmail.com>
2018-11-15 17:38:08 +0200

    changes

--- task_4/tests.py
+++ task_4/tests.py
@@ -1,7 +1,24 @@
+'''tests.py
+
+This test script is intended to looping of 'check' test as many times as many
+numbers in the 'mapping' dictionary for the given letter.
+
+Arguments:
+    This script requires one argument (letter)
+    letter: Have to write 'a' or 'b' or 'c' .
+
+Run script under easypy:
+    $ python tests.py --letter 'write "a" or "b" or "c"'
+
+'''
+
+__author__ = 'Yurii Kobuk'
+
+import argparse
+import sys
 from ats import aetest
 
 from ats.aetest import loop, test, setup
-
 
 mapping = {
     'a': (1, 3, 4, 5, 6, 7, 8),
@@ -11,6 +28,10 @@ mapping = {
 
 
 class Test(aetest.Testcase):
+    '''Testcase that takes one letter and ckecks if the letter is on mapping.
+    If the letter in on mapping the check function will make looping as many times
+    as many numbers in the 'mapping dictionary for the given number'
+    '''
 
     @setup
     def setup(self, letter):
@@ -26,4 +47,7 @@ class Test(aetest.Testcase):
 
 
 if __name__ == '__main__':
-    aetest.main()
+    parser = argparse.ArgumentParser(description="Write a letter")
+    parser.add_argument('--letter', type=str, required=True)
+    args, sys.argv[1:] = parser.parse_known_args(sys.argv[1:])
+    aetest.main(letter=args.letter)
