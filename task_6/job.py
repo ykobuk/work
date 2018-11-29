0d3ed47f42baea99547e2a0bae4f812becef7a98
asstelite <kobukstr@gmail.com>
2018-11-29 10:29:53 +0200

    Update job.py

--- task_6/job.py
+++ task_6/job.py
@@ -1,11 +1,5 @@
 from ats import easypy
 
-from pyats.topology import loader
-
-testbed = loader.load('testbed.yaml')
-directory = testbed.custom.directory
-report_directory = testbed.custom['report_directory']
-
 
 def main():
-    easypy.run(testscript='test.py', directory=directory, report_directory=report_directory)
+    easypy.run(testscript='test.py')
