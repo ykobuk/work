14ea6c4ba3a1c34a419190690b6090c687bcd1c9
asstelite <kobukstr@gmail.com>
2018-11-29 11:01:20 +0200

    ok

--- task_6/plugins/save.py
+++ task_6/plugins/save.py
@@ -11,8 +11,8 @@ logger.setLevel(logging.INFO)
 class CopyReport(BasePlugin):
     def __init__(self, directory, *args, **kwargs):
         super().__init__(*args, **kwargs)
-        self._source = None
-        self._dest = None
+        self._source = ''
+        self._dest = ''
 
     def post_job(self, job):
         self._source = job.testbed.custom['directory']
